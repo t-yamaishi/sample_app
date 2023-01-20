6e3a102b48187ba06fdb73d7974cb05760dad37f
yamaishi <yamakios8@icloud.com>
2023-01-20 06:57:42 +0000

    sessions_controller編集

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,8 @@ class SessionsController < ApplicationController
   end
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user&.authenticate(params[:session][:password])
-    #if user && user.authenticate(params[:session][:password])
+    #if user&.authenticate(params[:session][:password])
+    if user && user.authenticate(params[:session][:password])
       reset_session     
       log_in user
       redirect_to user
