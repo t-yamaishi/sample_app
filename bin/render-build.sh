0bef445abf72746d19aa168170d3d58ee3f1c8e6
yamaishi <yamakios8@icloud.com>
2023-01-27 04:31:25 +0000

    render-build.shを編集

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 # exit on error
+# set -o errexit
+# bundle install
+# bundle exec rails assets:precompile
+# bundle exec rails assets:clean
+# bundle exec rails db:migrate
+
 set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-bundle exec rails db:migrate
+bundle exec rails db:migrate:reset
+bundle exec rails db:seed
